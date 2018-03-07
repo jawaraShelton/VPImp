4da4227ccd5e783b9932ac7ccac16613d2f8fdbd
Jawara Shelton <jawara.shelton@gmail.com>
2018-03-07 10:36:42 -0700

    Add project files.

---
+++ VPImp/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace VPImp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txbSrc = new System.Windows.Forms.TextBox();
+            this.txbDst = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonUpdateDest = new System.Windows.Forms.Button();
+            this.buttonUpdateSource = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Begin Import";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(334, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit pImp";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txbSrc
+            // 
+            this.txbSrc.Location = new System.Drawing.Point(82, 342);
+            this.txbSrc.Name = "txbSrc";
+            this.txbSrc.Size = new System.Drawing.Size(202, 20);
+            this.txbSrc.TabIndex = 4;
+            // 
+            // txbDst
+            // 
+            this.txbDst.Location = new System.Drawing.Point(82, 367);
+            this.txbDst.Name = "txbDst";
+            this.txbDst.Size = new System.Drawing.Size(202, 20);
+            this.txbDst.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 345);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Import From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 370);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "To:";
+            // 
+            // buttonUpdateDest
+            // 
+            this.buttonUpdateDest.Location = new System.Drawing.Point(290, 366);
+            this.buttonUpdateDest.Name = "buttonUpdateDest";
+            this.buttonUpdateDest.Size = new System.Drawing.Size(25, 21);
+            this.buttonUpdateDest.TabIndex = 8;
+            this.buttonUpdateDest.Text = "...";
+            this.buttonUpdateDest.UseVisualStyleBackColor = true;
+            this.buttonUpdateDest.Click += new System.EventHandler(this.buttonUpdateDest_Click);
+            // 
+            // buttonUpdateSource
+            // 
+            this.buttonUpdateSource.Location = new System.Drawing.Point(290, 342);
+            this.buttonUpdateSource.Name = "buttonUpdateSource";
+            this.buttonUpdateSource.Size = new System.Drawing.Size(25, 20);
+            this.buttonUpdateSource.TabIndex = 9;
+            this.buttonUpdateSource.Text = "...";
+            this.buttonUpdateSource.UseVisualStyleBackColor = true;
+            this.buttonUpdateSource.Click += new System.EventHandler(this.buttonUpdateSource_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(0, -5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(334, 435);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(334, 450);
+            this.Controls.Add(this.buttonUpdateSource);
+            this.Controls.Add(this.buttonUpdateDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbDst);
+            this.Controls.Add(this.txbSrc);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "P.Imp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txbSrc;
+        private System.Windows.Forms.TextBox txbDst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonUpdateDest;
+        private System.Windows.Forms.Button buttonUpdateSource;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ VPImp/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+
+namespace VPImp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statusStrip1.Refresh();
+
+            txbSrc.Text = Properties.Settings.Default.ImageSource;
+            txbDst.Text = Properties.Settings.Default.ImageDestination;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String src = Properties.Settings.Default.ImageSource;
+            String dst = Properties.Settings.Default.ImageDestination;
+
+            PImp(src, dst);
+        }
+
+        private void PImp(String src, String dst)
+        {
+            
+            String exts = Properties.Settings.Default.ImageExtensions;
+            String[] folders = Directory.GetDirectories(src);
+
+            foreach (string nbcLock in folders)
+                PImp(nbcLock, dst);
+
+            toolStripStatusLabel1.Text = "Reading in file list for " + src;
+            statusStrip1.Refresh();
+            String[] files = Directory.GetFiles(src);
+
+            toolStripStatusLabel1.Text = "Importing Images...";
+            statusStrip1.Refresh();
+
+            foreach (string nbcLock in files)
+                if (exts.Contains(nbcLock.Substring(nbcLock.Length - 3, 3).ToUpper()))
+                {
+                    DateTime? d = DateTaken(nbcLock);
+
+                    String imageSortFolder = dst + "\\" + d.Value.Year.ToString() + "." + d.Value.Month.ToString("00") + "." + d.Value.Day.ToString("00");
+                    if (!Directory.Exists(imageSortFolder))
+                        Directory.CreateDirectory(imageSortFolder);
+
+                    String cOut = (nbcLock + " => " + imageSortFolder + "\\" + Path.GetFileName(nbcLock));
+                    if (cOut.Length > 118)
+                        cOut = cOut.Substring(0, 118);
+
+                    toolStripStatusLabel2.Text = cOut;
+                    statusStrip1.Refresh();
+
+                    try
+                    {
+                        File.Move(nbcLock, imageSortFolder + "\\" + Path.GetFileName(nbcLock));
+                    }
+                    catch
+                    {
+                        File.Move(nbcLock, imageSortFolder + "\\" + Renamed(nbcLock));
+                    }
+                }
+
+            Console.WriteLine("Checking to see if Folder is now empty...");
+            if (IsEmpty(src))
+            {
+                toolStripStatusLabel1.Text = "Folder is empty. Deleting.";
+                statusStrip1.Refresh();
+
+                Directory.Delete(src);
+            }
+
+            pictureBox1.Image = null;
+            pictureBox1.Refresh();
+
+            toolStripStatusLabel1.Text = "Import Complete.";
+            statusStrip1.Refresh();
+        }
+
+        private static String Renamed(String nbcLock)
+        {
+            int fLen = Path.GetFileName(nbcLock).Length;
+            String fExt = Path.GetFileName(nbcLock).Substring(fLen - 3, 3);
+            String fNym = Path.GetFileName(nbcLock).Substring(0, fLen - 5);
+            String fDst = fNym + DateTime.Now.ToString("H.mm.ss.ffff") + "." + fExt;
+
+            return (fDst);
+        }
+
+        private static Boolean IsEmpty(String targetDirectory)
+        {
+            return ((Directory.GetFiles(targetDirectory).Length + Directory.GetDirectories(targetDirectory).Length) == 0);
+        }
+
+        public DateTime? DateTaken(String fNym)
+        {
+            Regex r = new Regex(":");
+            string dateTaken = "";
+
+            using (FileStream fs = new FileStream(fNym, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromFile(fNym))
+            {
+                pictureBox1.Image = myImage;
+                pictureBox1.Refresh();
+
+                PropertyItem propItem;
+
+                try
+                { 
+                    propItem = myImage.GetPropertyItem(0x9003);
+                    dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                }
+                catch
+                {
+                    dateTaken = "2/19/1936";
+                }
+                Console.WriteLine(dateTaken);
+            }
+
+            return DateTime.Parse(dateTaken);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonUpdateSource_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            DialogResult result = this.folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string foldername = this.folderBrowserDialog1.SelectedPath;
+
+                txbSrc.Text = foldername;
+                txbSrc.Refresh();
+
+                Properties.Settings.Default.ImageSource = foldername;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void buttonUpdateDest_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1.ShowNewFolderButton = true;
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            DialogResult result = this.folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string foldername = this.folderBrowserDialog1.SelectedPath;
+
+                txbDst.Text = foldername;
+                txbDst.Refresh();
+
+                Properties.Settings.Default.ImageDestination = foldername;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
